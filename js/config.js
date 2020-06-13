8b120c1c67f2973a6e683ec04a5506d072890d19
Qi Wang <wangqi0523@gmail.com>
2020-06-13 18:50:48 -0500

    added invokeUrl in config.js

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@
         region: 'us-west-2' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod,
+        invokeUrl: 'https://ocskcy5k50.execute-api.us-west-2.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod,
     }
 };
